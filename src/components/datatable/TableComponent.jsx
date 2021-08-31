4881d8cf42086c2997b7984a5bf7884fc5f6c70d
Han <farhanafriza24@gmail.com>
2021-08-31 15:56:30 +0700

    add function to get key for dynamic key to get data from json data and handler if data is empty

--- src/components/datatable/TableComponent.jsx
+++ src/components/datatable/TableComponent.jsx
@@ -15,6 +15,17 @@ class TableComponent extends React.Component {
         return tableHeader
     }
 
+    keyForGetData = () => {
+        const keyData = [];
+    
+        for(var key in this.props.data[0]) {
+            if (key === 'id') continue
+
+            keyData.push(key);
+        }
+        return keyData
+    }
+
     handleRemoveData = (id) => {
         if (window.confirm('Are you sure you want to remove this ?')) {
             this.props.removeFunc({ id: id })
@@ -27,25 +38,23 @@ class TableComponent extends React.Component {
 
     render(){
         return (
-            <Table striped bordered hover size="sm">
+            <Table className="mt-2" striped bordered hover size="sm">
                 <thead>
                     <tr>
                         {this.tableHeader().map(author => <th key={author}>{author}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {this.props.data.map((data, i) => (
+                    {this.props.data.length > 0 ? this.props.data.map((data, i) => (
                         <tr key={data.id}>
                             <td>{i + 1}</td>
-                            <td>{data.name}</td>
-                            <td>{data.email}</td>
-                            <td>{data.status}</td>
+                                {this.keyForGetData().map((key,i) => <td key={key + i + '_td_table'}>{data[key]}</td>)}
                             <td>
                                 <Button variant="outline-primary" onClick={() => this.handleUpdateData(data)} size="sm">Update</Button>&nbsp;
                                 <Button variant="outline-danger" onClick={() => this.handleRemoveData(data.id)} size="sm">Danger</Button>
                             </td>
                         </tr>
-                    ))}
+                    )) : <tr><td className="text-center">Tidak ada data</td></tr>}
                 </tbody>
             </Table>
         )
